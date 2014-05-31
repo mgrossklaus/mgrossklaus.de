c0ab43ff53b5177a60e4dee77bd7ff2728df02e7
mgrsskls <mail@mgrossklaus.de>
2014-05-31 15:53:01 +0200

    Image bereits im head einfuegen

--- application.js
+++ application.js
@@ -11,13 +11,10 @@
 /*global  window, Modernizr */
 document.addEventListener('DOMContentLoaded', function(event) {
 
-	var mg			= document.getElementById('mg'),
-			mgName	= document.getElementById('name'),
+	var mgName	= document.getElementById('name'),
 			mgBody	= document.getElementById('mgrossklaus-de'),
 			mgBg		= document.getElementById('bg'),
-			scrollY	= window.pageYOffset,
-			bgImg		= new Image(),
-			loaded	= false;
+			scrollY	= window.pageYOffset;
 
 	window.requestAnimFrame = (function(){
 		return  window.requestAnimationFrame       ||
@@ -27,15 +24,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
 			window.setTimeout(callback, 1000 / 60);
 		};
 	}());
-
-	function loadHandler() {
-		if(loaded) {
-			return;
-		}
-		loaded = true;
-
-		mg.className += ' mg--loaded';
-	}
 
 	function updateLayout(scrollY) {
 		if(scrollY <= 500) {
@@ -71,13 +59,5 @@ document.addEventListener('DOMContentLoaded', function(event) {
 		})();
 	}
 
-	bgImg.onload = loadHandler;
-	bgImg.src = 'mg.jpg';
-
-	if(bgImg.complete) {
-		loadHandler();
-	}
-
 	document.getElementById('lazy-css').media = 'screen';
-	document.getElementById('lazy-font').media = 'screen';
 });
