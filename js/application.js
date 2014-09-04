a497e33ee129fb957b9fe33d2e668e53566d0cea
mgrsskls <mail@mgrossklaus.de>
2014-09-04 20:01:17 +0200

    css lazyloading

--- js/application.js
+++ js/application.js
@@ -9,6 +9,8 @@
 					windows:  true,
 					nomen:    true*/
 /*global  window */
+var assetUrl = 'http://files.mgrossklaus.de';
+
 document.addEventListener('DOMContentLoaded', function(event) {
 
 	var mg			= document.getElementById('mg'),
@@ -73,9 +75,27 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 	bgImg.onload = loadHandler;
 
-	bgImg.src = 'http://files.mgrossklaus.de/mg.jpg';
+	bgImg.src = assetUrl + '/mg.jpg';
 
 	if(bgImg.complete) {
 		loadHandler();
 	}
 });
+
+/*!
+loadCSS: load a CSS file asynchronously.
+[c]2014 @scottjehl, Filament Group, Inc.
+Licensed MIT
+*/
+
+var ss = window.document.createElement( "link" );
+ss.rel = "stylesheet";
+ss.href = assetUrl + '/css/styles.min.css';
+// temporarily, set media to something non-matching to ensure it'll fetch without blocking render
+ss.media = "only x";
+// inject link
+ref.parentNode.insertAfter( ss, document.getElementsByTagName('style')[0] );
+// set media back to `all` so that the styleshet applies once it loads
+setTimeout( function(){
+	ss.media = media || "all";
+} );
