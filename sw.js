f39228567f8ede6a2ed1662fe857f34ff1475e27
mgrsskls <mail@mgrossklaus.de>
2016-10-27 11:44:43 +0200

    minor css fix for project links

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = 21,
+const CACHE_VERSION = 22,
       CACHE_NAME    = 'mgrsskls',
       CURRENT_CACHE = CACHE_NAME + '-v' + CACHE_VERSION,
       URLS_TO_CACHE = [
